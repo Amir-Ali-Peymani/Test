849333faa06d7ee2dfcfa9a8f4d145ecbabf6b49
DarthBane <amiralipeymanispacelove@gmail.com>
2024-10-10 14:30:24 +0330

    Body Error Response body

---
+++ test/src/main/java/com/example/test/exception/BodyErrorResponse.java
@@ -0,0 +1,15 @@
+package com.example.test.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BodyErrorResponse {
+
+    private String message;
+
+
+}
